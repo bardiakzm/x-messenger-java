966f81feaec9d359da6694fffcc83514179fa7ed
bardia <bardiakzm@gmail.com>
2024-06-08 15:30:11 +0330

    lazyCommit

---
+++ Packet.java
@@ -0,0 +1,8 @@
+public class Packet {
+    String header = "enmpty header";
+    Object data = "empty data";
+    Packet(String header,Object data){
+        this.header = header;
+        this.data = data;
+    }
+}
