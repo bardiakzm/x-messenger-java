045ec3cf75e2b2d98439f3246af6735bfb671d3c
bardia <bardiakzm@gmail.com>
2024-06-08 17:09:10 +0330

    lazyCommit

--- Main.java
+++ Main.java
@@ -1,15 +1,16 @@
 
 import java.io.Serializable;
+import javax.swing.SwingUtilities;
 
 
 public class Main implements Serializable{
     final static String key = "uyaq2hAbK7tJXfqs";
     public static void main(String[] args) {
-        Packet.sendPacket("header", "hi",key);
-        // SwingUtilities.invokeLater(new Runnable() {
-        //     public void run() {
-        //         new LoginPage();
-        //     }
-        // });
+        // Packet.sendPacket("header", "hi",key);
+        SwingUtilities.invokeLater(new Runnable() {
+            public void run() {
+                new LoginPage();
+            }
+        });
     }
 }
