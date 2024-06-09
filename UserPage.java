7170e98093a35a74e03ac6a1e8237b61af40394b
bardia <bardiakzm@gmail.com>
2024-06-09 12:31:03 +0330

    lazyCommit

--- UserPage.java
+++ UserPage.java
@@ -9,23 +9,31 @@ public class UserPage extends JFrame {
     private User user;
     private List<Tweet> followingTweets;
     private List<Tweet> randomTweets;
-    
+    private List<Tweet> savedTweets;
+
     private JPanel tweetPanel;
 
     public UserPage(User user, List<Tweet> followingTweets, List<Tweet> randomTweets) {
         this.user = user;
         this.followingTweets = followingTweets;
         this.randomTweets = randomTweets;
-
+        this.savedTweets = new ArrayList<>();
         setTitle("User Page");
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         setLayout(new BorderLayout());
 
-        // Create buttons
+        // Create top panel with buttons
+        JPanel topPanel = new JPanel(new FlowLayout(FlowLayout.LEADING));
+        JButton profileButton = new JButton("Profile");
+        JButton savedTweetsButton = new JButton("Saved Tweets");
+        topPanel.add(profileButton);
+        topPanel.add(Box.createHorizontalGlue());
+        topPanel.add(savedTweetsButton);
+
+        // Create button panel
         JPanel buttonPanel = new JPanel();
         JButton followingButton = new JButton("Following Tweets");
         JButton randomButton = new JButton("Random Tweets");
-
         buttonPanel.add(followingButton);
         buttonPanel.add(randomButton);
 
@@ -34,7 +42,8 @@ public class UserPage extends JFrame {
         tweetPanel.setLayout(new BoxLayout(tweetPanel, BoxLayout.Y_AXIS));
         JScrollPane scrollPane = new JScrollPane(tweetPanel);
 
-        add(buttonPanel, BorderLayout.NORTH);
+        add(topPanel, BorderLayout.NORTH);
+        add(buttonPanel, BorderLayout.SOUTH);
         add(scrollPane, BorderLayout.CENTER);
 
         // Add action listeners
@@ -44,16 +53,22 @@ public class UserPage extends JFrame {
                 showTweets(followingTweets);
             }
         });
-
         randomButton.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
                 showTweets(randomTweets);
             }
         });
+        savedTweetsButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                showTweets(savedTweets);
+            }
+        });
+
 
         // Show the window
-        setSize(600, 400);
+        setSize(800, 810);
         setLocationRelativeTo(null);
         setVisible(true);
     }
@@ -61,26 +76,44 @@ public class UserPage extends JFrame {
     private void showTweets(List<Tweet> tweets) {
         tweetPanel.removeAll();
         for (Tweet tweet : tweets) {
-            tweetPanel.add(new JLabel(tweet.publisherid + ": " + tweet.text + " (Likes: " + tweet.likes + ")"));
+            JPanel tweetContainer = new JPanel(new BorderLayout());
+            JLabel tweetLabel = new JLabel(tweet.publisherid + ": " + tweet.text + " (Likes: " + tweet.likes + ")");
+            JPanel buttonPanel = new JPanel();
+            JButton saveButton = new JButton("Save");
+            JButton likeButton = new JButton("Like");
+            buttonPanel.add(saveButton);
+            buttonPanel.add(likeButton);
+            tweetContainer.add(tweetLabel, BorderLayout.CENTER);
+            tweetContainer.add(buttonPanel, BorderLayout.EAST);
+            tweetPanel.add(tweetContainer);
+
+            // Add action listeners for save and like buttons
+            saveButton.addActionListener(new ActionListener() {
+                @Override
+                public void actionPerformed(ActionEvent e) {
+                    saveTweet(tweet);
+                }
+            });
+            likeButton.addActionListener(new ActionListener() {
+                @Override
+                public void actionPerformed(ActionEvent e) {
+                    likeTweet(tweet);
+                }
+            });
         }
         tweetPanel.revalidate();
         tweetPanel.repaint();
     }
 
-    public static void main(String[] args) {
-        User dummyUser = new User("dummyUser", "password", "Dummy User", "dummy@example.com", "1234567890", 30, "This is a dummy user.");
-        List<Tweet> followingTweets = new ArrayList<>();
-        List<Tweet> randomTweets = new ArrayList<>();
-        
-        // Add some dummy tweets
-        User dummyPublisher1 = new User("publisher1", "password", "Publisher 1", "pub1@example.com", "0987654321", 25, "This is publisher 1.");
-        User dummyPublisher2 = new User("publisher2", "password", "Publisher 2", "pub2@example.com", "0123456789", 28, "This is publisher 2.");
+    private void saveTweet(Tweet tweet) {
+        savedTweets.add(tweet);
+        JOptionPane.showMessageDialog(this, "Tweet saved successfully!");
+    }
 
-        followingTweets.add(new Tweet(dummyPublisher1, "Following tweet 1"));
-        followingTweets.add(new Tweet(dummyPublisher1, "Following tweet 2"));
-        randomTweets.add(new Tweet(dummyPublisher2, "Random tweet 1"));
-        randomTweets.add(new Tweet(dummyPublisher2, "Random tweet 2"));
-
-        new UserPage(dummyUser, followingTweets, randomTweets);
+    private void likeTweet(Tweet tweet) {
+        tweet.likes++;
+        showTweets(followingTweets);
+        showTweets(randomTweets);
+        showTweets(savedTweets);
     }
 }
